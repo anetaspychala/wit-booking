97ef1a3929eaa9743366a0a41dafc5f570f054d9
anetaspychala <131193224+anetaspychala@users.noreply.github.com>
2023-09-19 20:53:28 +0200

    checks

--- src/main/java/pl/sdacademy/booking/OwnerApp.java
+++ src/main/java/pl/sdacademy/booking/OwnerApp.java
@@ -1,7 +1,7 @@
 package pl.sdacademy.booking;
 
+import pl.sdacademy.booking.controller.EventController;
 import pl.sdacademy.booking.controller.ItemController;
-
 import java.util.Scanner;
 
 public class OwnerApp {
@@ -10,13 +10,19 @@ public class OwnerApp {
 
         System.out.println("Witam w salonie XXX");
         System.out.println("Menu:");
-        System.out.println("1: - pokaz katalog");
+        System.out.println("1: - pokaz katalog produktow");
+        System.out.println("2: - pokaz terminarz sesji");
 
         Scanner scanner = new Scanner(System.in);
         int inputValue = scanner.nextInt();
         if (inputValue == 1) {
             new ItemController().presentCatalog();
         }
+        if (inputValue == 2) {
+            new EventController().presentEventSchedule();
+        }
+
 
     }
+
 }
