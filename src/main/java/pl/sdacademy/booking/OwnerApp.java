9ed1e59d32b3e7f119573ca8d30237b6528d3687
anetaspychala <131193224+anetaspychala@users.noreply.github.com>
2023-09-19 11:51:58 +0200

    Zad Domowe

--- src/main/java/pl/sdacademy/booking/OwnerApp.java
+++ src/main/java/pl/sdacademy/booking/OwnerApp.java
@@ -11,11 +11,14 @@ public class OwnerApp {
         System.out.println("Witam w salonie XXX");
         System.out.println("Menu:");
         System.out.println("1: - pokaz katalog");
+        System.out.println("2: - pokaz wydarzenia");
 
         Scanner scanner = new Scanner(System.in);
         int inputValue = scanner.nextInt();
         if (inputValue == 1) {
-            new ItemController().presentCatalog();
+           // new ItemController(itemService, eventService).presentCatalog();
+        } else if (inputValue == 2) {
+            //new ItemController(itemService, eventService).presentEvents();
         }
 
     }
